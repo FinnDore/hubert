5e3df22624083489a4105e61a134513def14d01e
Finn <34718806+FinnDore@users.noreply.github.com>
2022-08-26 14:35:39 +0100

    🐠

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,10 @@ const Home: NextPage = () => {
         <div className="relative h-screen w-screen overflow-hidden">
             <div className="🐡 text-zinc-100 z-10 w-max max-w-full text-5xl md:text-6xl max-w-screen">{hubertActuallySays}</div>
             <Image src="/hubert.jpeg" alt="Hubert" layout="fill"></Image>
+            <a href="https://github.com/finndore/hubert" className="py-4 px-4 flex z-10 bottom-0 right-0 absolute            ">
+                {' '}
+                <Image src="/github-light.png" alt="github" layout="fixed" height={20} width={20}></Image>
+            </a>
         </div>
     );
 };
